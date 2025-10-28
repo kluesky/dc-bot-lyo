a04627c26903fb8463606977f84b0b4843ca722a
Lyora <140289431+kluesky@users.noreply.github.com>
2025-10-29 06:59:52 +0700

    Delete keep-alive.js

--- keep-alive.js
+++
@@ -1,8 +0,0 @@
-// Keep bot alive
-import http from 'http';
-const server = http.createServer((req, res) => {
-  res.writeHead(200);
-  res.end('Bot is alive!');
-});
-server.listen(3000);
-console.log('Keep-alive server running on port 3000');
